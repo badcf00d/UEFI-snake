140cbffa482b3c76bf7daad0f9e1eba75cd603a9
Peter Frost <mail@pfrost.me>
2020-03-08 21:31:11 +0000

    Fixed CreateEvent function call

--- src/uefi.asm
+++ src/uefi.asm
@@ -19,6 +19,7 @@ _start:
     mov [ptrUEFI], rsp
     mov [ptrImageHandle], rcx
     mov [ptrSystemTable], rdx
+    sub rsp, 0x8                   ; align stack
 
     mov rcx, [rdx + EFI_SYSTEM_TABLE.ConOut]                    ; ConOut is a EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL for the default console
     call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.ClearScreen]    ; Clear the default console
@@ -27,10 +28,7 @@ _start:
 
     mov rcx, helloWorld
     call printString
-
-    call getFrameBuffer             ; get a framebuffer to draw on
-    mov rax, [ptrFrameBuffer]       ; load the framebuffer into rdx
-    push rax
+    call getFrameBuffer
 
 
     mov rcx, EVT_TIMER
@@ -39,7 +37,7 @@ _start:
     mov r9, 0
     lea rbx, [ptrTimerEvent]
     push rbx
-
+    sub rsp, 0x20
     mov rax, [ptrSystemTable]
     mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
     call [rax + EFI_BOOT_SERVICES.CreateEvent]
@@ -47,35 +45,32 @@ _start:
     cmp rax, EFI_SUCCESS
     jne errorCode
 
-    pop r10
-
-    jmp $
-
     mov rcx, [ptrTimerEvent]
     mov rdx, TimerPeriodic
-    mov r8,  56000                         ; 56 ms (about 18 Hz) in units of 100 nanoseconds
+    mov r8, 56000                         ; 56 ms (about 18 Hz) in units of 100 nanoseconds
     mov rax, [ptrSystemTable]
     mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
     call [rax + EFI_BOOT_SERVICES.SetTimer]
+
     cmp rax, EFI_SUCCESS
     jne errorCode
 
 
 delayUntilTick:
-
     mov rcx, 1
-    mov rdx, [ptrTimerEvent]
+    lea rdx, [ptrTimerEvent]
+    lea r8, [ptrEventIndex]
     mov rax, [ptrSystemTable]
     mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
     call [rax + EFI_BOOT_SERVICES.WaitForEvent]
     cmp rax, EFI_SUCCESS
     jne errorCode
-    ; r8 will have a pointer to the index of the event in the events that got signaled
 
-    pop rax
+
+    mov rax, [ptrFrameBuffer]
     mov [rax], dword 0xFFFF0000     ; write 32-bits to the framebuffer (A,R,G,B)
     add rax, 4
-    push rax
+    mov [ptrFrameBuffer], rax
 
     jmp delayUntilTick                           ; loop forever
 
@@ -93,5 +88,6 @@ section .data   ; contains the program data
     ptrFrameBuffer  dq  0
     ptrInterface    dq  0
     ptrTimerEvent   dq  0
+    ptrEventIndex   dq  0
 
 dataSize equ $ - $$
